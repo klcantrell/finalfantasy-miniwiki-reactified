e028d8a66c4d404e0c6c8c27bcede57b829a1a39
Kalalau <cantrellkalalau@gmail.com>
2018-06-10 22:38:20 -0400

    app now gets character data on mounting

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Nav from './Nav';
 import Characters from './Characters';
+import { fetchData, fetchImg } from '../utils';
 
 class App extends Component {
+  componentDidMount() {
+    fetchData('data/data.json')
+      .then(res => {
+        this.data = res;
+      })
+  }
+
   render() {
     return (
       <div style={{color: "white"}}>
         <Nav />
         <Route exact path="/" component={() => <p>Hello World</p>}/>
-        <Route path="/:characterName" render={props => <Characters {...props} />}/>
+        <Route path="/:characterName" render={props => <Characters {...props} data={this.data} />}/>
       </div>
     );
   }
