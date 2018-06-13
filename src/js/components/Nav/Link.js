44c40f539825ce345c3a0a4ab7a920277861dc47
Kalalau <cantrellkalalau@gmail.com>
2018-06-12 20:43:11 -0400

    Link component: * wraps the use of react router's NavLink * maintains full image loaded state so that placeholder can be phased out

--- src/js/components/Nav/Link.js
+++ src/js/components/Nav/Link.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Link = ({path, pic}) => {
-  return (
-    <NavLink className="nav__hero-container" to={path}>
-      <img className="nav__hero" srcSet={pic.srcSet} src={pic.src} />
-    </NavLink>
-  );
-};
+class Link extends Component {
+  state = {
+    fullLoaded: false,
+  };
+
+  fullImageLoaded = () => {
+    this.setState({
+      fullLoaded: true,
+    })
+  }
+
+  render() {
+    const { path, pic } = this.props;
+    const { fullLoaded } = this.state;
+    return (
+      <NavLink activeStyle={{	borderBottom: "8px solid rgba(186, 213, 234, 1)" }} className="nav__hero-container" to={path}>
+        <img className="nav__hero-preview" src={pic.placeholder} />
+        <img onLoad={this.fullImageLoaded} className={`nav__hero ${fullLoaded ? 'nav__hero--loaded' : ''}`} sizes='(min-width: 750px) 240px, 150px' srcSet={pic.srcSet} src={pic.src} />
+      </NavLink>
+    );
+  }
+}
+
 
 export default Link;
