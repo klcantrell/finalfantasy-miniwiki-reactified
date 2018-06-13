65873b8b8c340323e56528db997b42fb0a8b5b54
Kalalau <cantrellkalalau@gmail.com>
2018-06-12 20:38:25 -0400

    moved characterName route render inside of Characters component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,22 +2,15 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Nav from './Nav';
 import Characters from './Characters';
-import { fetchData, fetchImg } from '../utils';
 
 class App extends Component {
-  componentDidMount() {
-    fetchData('data/data.json')
-      .then(res => {
-        this.data = res;
-      })
-  }
 
   render() {
     return (
       <div style={{color: "white"}}>
         <Nav />
-        <Route exact path="/" component={() => <p>Hello World</p>}/>
-        <Route path="/:characterName" render={props => <Characters {...props} data={this.data} />}/>
+        <Route exact path="/" component={() => <h3 className="info__header">Select a character</h3>}/>
+        <Characters />
       </div>
     );
   }
