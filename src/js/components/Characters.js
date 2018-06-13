905882f448d61e9c1699f67f9683e496e4779026
Kalalau <cantrellkalalau@gmail.com>
2018-06-12 20:39:41 -0400

    added Characters component: * maintains character data * feeds data to a Character component when route renders a particular character

--- src/js/components/Characters.js
+++ src/js/components/Characters.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Character from './Character';
+import { fetchData, fetchImg } from '../utils';
 
 class Characters extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      characterData: this.props.data,
+      charactersData: undefined
     };
   }
 
+  componentDidMount() {
+    if (!this.state.charactersData) {
+      fetchData('data/data.json')
+        .then(res => {
+          this.setState({
+            charactersData: res,
+          });
+        });
+    }
+  }
+
+  getCharacterImg = (character, imgUrl) => {
+    fetchImg(imgUrl)
+      .then(blob => {
+        const charactersData = { ...this.state.charactersData };
+        charactersData[character].imgSrc = URL.createObjectURL(blob);
+        this.setState({
+          charactersData,
+        });
+      });
+  }
+
   render() {
-    const { match: {params: { characterName } } } = this.props;
-    const key = characterName.split('-')[0];
-    const data = this.state.characterData[key];
-    return <Character {...data} />
+    const { charactersData } = this.state;
+    if (charactersData) {
+      return (
+        <Route path="/:characterName" render={props => <Character {...props} charactersData={charactersData} getImg={this.getCharacterImg} />}/> 
+      );
+    } else {
+      return <div className="info">'Loading...'</div>;
+    }
   }
 }
 
