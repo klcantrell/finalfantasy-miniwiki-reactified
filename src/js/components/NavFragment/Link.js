63f1345a277aeb4530da509d44efbe0410d11985
Kalalau <cantrellkalalau@gmail.com>
2018-06-14 18:03:51 -0400

    prettied up formatting of props

--- src/js/components/NavFragment/Link.js
+++ src/js/components/NavFragment/Link.js
@@ -16,7 +16,11 @@ class Link extends Component {
     const { path, pic } = this.props;
     const { fullLoaded } = this.state;
     return (
-      <NavLink activeStyle={{	borderBottom: "8px solid rgba(186, 213, 234, 1)" }} className="nav__hero-container" to={path}>
+      <NavLink 
+        activeStyle={{	borderBottom: "8px solid rgba(186, 213, 234, 1)" }} 
+        className="nav__hero-container" 
+        to={path}
+      >
         <img className="nav__hero-preview" src={pic.placeholder} />
         <img onLoad={this.fullImageLoaded} className={`nav__hero ${fullLoaded ? 'nav__hero--loaded' : ''}`} sizes='(min-width: 750px) 240px, 150px' srcSet={pic.srcSet} src={pic.src} />
       </NavLink>
