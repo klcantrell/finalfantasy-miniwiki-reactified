8053db2aac44e2c7312e602c2229c3b6553903b5
Kalalau <cantrellkalalau@gmail.com>
2018-06-14 18:02:30 -0400

    refactored so that it can fire a sync with pagination components

--- src/js/components/Character/Character.js
+++ src/js/components/Character/Character.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LoadingSpinner from '../LoadingSpinner';
 
 const formatName = name => {
   return name.replace('-', ' ');
 };
 
-const Character = ({ match: { params }, charactersData, getImg }) => {
-  const { characterName } = params;
-  const { hometown, sprite, weapon, imgSrc } = charactersData[characterName];
-  if (!imgSrc) {
-    getImg(characterName, require(`../../../images/${sprite}`).src);
+class Character extends Component {
+  componentDidMount() {
+    const { syncPage, charactersData, match: { params: { characterName } } } = this.props;
+    syncPage(charactersData[characterName].page);
   }
-  return (
-    <div className="info">
-      <h1 className="info__header">{formatName(characterName)}</h1>
-      <div className="info__details">
-        <div className="info__details-item">
-          <h3>Hometown</h3>
-          <p>{hometown}</p>
-        </div>
-        <div className="info__sprite">
-          <h3>Original Sprite</h3>
-          {imgSrc ? <img src={imgSrc} alt={sprite} /> : <LoadingSpinner />}
-        </div>
-        <div className="info__details-item">
-          <h3>Default Weapon</h3>
-          <p>{weapon}</p>
+
+  render() {
+    const { match: { params: { characterName } }, charactersData, getSpriteImg } = this.props;
+    const { hometown, sprite, weapon, imgSrc } = charactersData[characterName];
+    if (!imgSrc) {
+      getSpriteImg(characterName, require(`../../../images/${sprite}`).src);
+    }
+    return (
+      <div className="info">
+        <h1 className="info__header">{formatName(characterName)}</h1>
+        <div className="info__details">
+          <div className="info__details-item">
+            <h3>Hometown</h3>
+            <p>{hometown}</p>
+          </div>
+          <div className="info__sprite">
+            <h3>Original Sprite</h3>
+            {imgSrc ? <img src={imgSrc} alt={sprite} /> : <LoadingSpinner />}
+          </div>
+          <div className="info__details-item">
+            <h3>Default Weapon</h3>
+            <p>{weapon}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Character;
